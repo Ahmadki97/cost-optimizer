fa1ac93131b0dfb6e0289dfec23a54ecfba0d20e
Ahmadki97 <ahmad.s.kirata97@gmail.com>
2025-09-23 19:18:27 +0800

    Remove test files from repo

--- test.py
+++
@@ -1,4 +0,0 @@
-import boto3
-client = boto3.Session(profile_name='devops-personal').client("pricing", region_name="us-east-1")
-response = client.get_products(ServiceCode="AmazonEC2", MaxResults=100)
-print(response['PriceList'][0])
